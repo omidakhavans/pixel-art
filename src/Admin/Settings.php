e7964977b6529d6bbe6841a1b7862af469b1e43e
Omid <omidakingroid@gmail.com>
2024-05-30 02:33:54 +0300

    Adjust dependency of admin script

--- src/Admin/Settings.php
+++ src/Admin/Settings.php
@@ -30,7 +30,7 @@ final class Settings implements Initializer {
 	 * Enqueue admin scripts and styles.
 	 */
 	public static function enqueue_admin_scripts() {
-		wp_enqueue_script( 'pixel-art-admin-script', PIXEL_ART_URL . 'build/admin.js', array( 'wp-element' ), null, true );
+		wp_enqueue_script( 'pixel-art-admin-script', PIXEL_ART_URL . 'build/admin.js', array( 'wp-api-fetch','wp-element', 'wp-url' ), null, true );
 		wp_enqueue_style( 'pixel-art-admin-style', PIXEL_ART_URL . 'assets/admin.css' );
 	}
 
