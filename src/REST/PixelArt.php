791a58a72897dd9795c2a35cd46c538233c1a370
Omid <omidakingroid@gmail.com>
2024-06-01 22:49:33 +0300

    Check and validate the json data on get and set of route

--- src/REST/PixelArt.php
+++ src/REST/PixelArt.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Pixel Art REST Routes.
+ *
+ * @package   pixel Art
+ * @copyright 2024 RBL
+ * @license   GNU General Public License 3.0
+ */
 
 namespace RBL\Pixel_Art\REST;
 
@@ -18,23 +25,23 @@ use RBL\Pixel_Art\REST\Base;
 class PixelArt extends Base {
 
 	/**
-     * Rest base name.
-     *
-     * @since 1.0
-     *
-     * @var string
-     */
+	 * Rest base name.
+	 *
+	 * @since 1.0
+	 *
+	 * @var string
+	 */
 	protected $rest_base = 'pixel-art';
 
 	/**
-     * Registers route.
-     *
-     * @since 1.0
-     *
-     * @see register_rest_route()
+	 * Registers route.
+	 *
+	 * @since 1.0
+	 *
+	 * @see register_rest_route()
 	 *
 	 * @return void
-     */
+	 */
 	public function register_routes(): void {
 		register_rest_route(
 			$this->namespace,
@@ -64,13 +71,13 @@ class PixelArt extends Base {
 	}
 
 	/**
-     * Save pixel art data to the database.
-     *
-     * @since 1.0
-     *
-     * @param WP_REST_Request $request Full data about the request.
-     * @return WP_REST_Response|WP_Error Response object on success, or WP_Error object on failure.
-     */
+	 * Save pixel art data to the database.
+	 *
+	 * @since 1.0
+	 *
+	 * @param WP_REST_Request $request Full data about the request.
+	 * @return WP_REST_Response|WP_Error Response object on success, or WP_Error object on failure.
+	 */
 	public function save_pixel_art( $request ) {
 		$pixels = $request->get_param( 'option' );
 
@@ -78,7 +85,15 @@ class PixelArt extends Base {
 			return new WP_Error( 'invalid_payload', __( 'Invalid payload', 'rbl-pixel-art' ), array( 'status' => 400 ) );
 		}
 
-        $this->cache_handler->set( 'pad_pixel_art', $pixels );
+		if ( ! is_string( $pixels ) ) {
+			$pixels = json_encode( $pixels );
+		}
+
+		if ( false === $pixels ) {
+			return new WP_Error( 'invalid_pixel_art_data', __( 'Error encoding option to JSON.', 'rbl-pixel-art' ), array( 'status' => 400 ) );
+		}
+
+		$this->cache_handler->set( 'pad_pixel_art', $pixels );
 
 		return rest_ensure_response( __( 'Pixel art saved', 'rbl-pixel-art' ), 200 );
 	}
@@ -93,16 +108,26 @@ class PixelArt extends Base {
 	 */
 	public function get_pixel_art( $request ) {
 		$pixels = $this->cache_handler->get( 'pad_pixel_art', array_fill( 0, 256, 'transparent' ) );
+
+		// Check if the retrieved data is a JSON string.
+		if ( ! is_string( $pixels ) || ! json_decode( $pixels ) ) {
+			$pixels = json_encode( $pixels );
+		}
+
+		if ( $pixels === false ) {
+			return new WP_Error( 'invalid_pixel_art_data', __( 'Error encoding data to JSON.', 'rbl-pixel-art' ), array( 'status' => 400 ) );
+		}
+
 		return rest_ensure_response( $pixels, 200 );
 	}
 
 	/**
-     * Retrieves the params for endpoint.
-     *
-     * @since 1.0
-     *
-     * @return array Parameters.
-     */
+	 * Retrieves the params for endpoint.
+	 *
+	 * @since 1.0
+	 *
+	 * @return array Parameters.
+	 */
 	public function get_collection_params(): array {
 		$query_params = array();
 
@@ -117,14 +142,14 @@ class PixelArt extends Base {
 	}
 
 	/**
-     * Permission check.
-     *
-     * @param WP_REST_Request $request Full data about the request.
-     *
-     * @since 1.0
-     *
-     * @return true|WP_Error
-     */
+	 * Permission check.
+	 *
+	 * @param WP_REST_Request $request Full data about the request.
+	 *
+	 * @since 1.0
+	 *
+	 * @return true|WP_Error
+	 */
 	public function permissions_check(
 		WP_REST_Request $request
 	) {
