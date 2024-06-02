c607070c63203765394cda4a30da6fc34acda731
Omid <omidakingroid@gmail.com>
2024-06-02 03:03:15 +0300

    Update webpack and package json rules

--- webpack.config.js
+++ webpack.config.js
@@ -6,34 +6,51 @@ const path = require( 'path' );
 /**
  * WordPress dependencies
  */
-const defaultConfig = require('@wordpress/scripts/config/webpack.config');
+const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
 
-module.exports = {
-  mode:'development',
-  ...defaultConfig,
-  entry: {
-    admin: './assets/admin.js',
-  },
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: '[name].js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-react'],
-          },
-        },
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
+const sharedConfig = {
+	...defaultConfig,
+	output: {
+		path: path.resolve( process.cwd(), 'build' ),
+		filename: '[name]/index.js',
+		chunkFilename: '[name]/index.js?v=[chunkhash]',
+		publicPath: '',
+	},
+	resolve: {
+		fallback: {
+			path: require.resolve( 'path-browserify' ),
+		},
+	},
+	module: {
+    ...defaultConfig.module,
+		rules: [
+      ...defaultConfig.module.rules,
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: [ '@babel/preset-react' ],
+					},
+				},
+			}
+		],
+	},
 };
+
+const Admin = {
+	...sharedConfig,
+	entry: {
+		admin: './assets/src/admin/index.js',
+	},
+};
+
+const Block = {
+	...sharedConfig,
+	entry: {
+		block: './src/Block/index.js',
+	},
+};
+
+module.exports = [ Admin, Block ];
