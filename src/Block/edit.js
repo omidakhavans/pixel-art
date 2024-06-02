787ac8074748d426cd798231cce80244ac5892c4
Omid <omidakingroid@gmail.com>
2024-06-02 03:08:50 +0300

    Implement JSLint to block functionality

--- src/Block/edit.js
+++ src/Block/edit.js
@@ -1,9 +1,32 @@
-export default function Edit() {
+/**
+ * WordPress dependencies
+ */
+import { useBlockProps,
+	InspectorControls,
+} from '@wordpress/block-editor';
+import ServerSideRender from '@wordpress/server-side-render';
+import {
+	PanelBody,
+	RangeControl,
+} from '@wordpress/components';
 
-
+export default function Edit( { attributes, setAttributes } ) {
+	const { size } = attributes;
 	const blockProps = useBlockProps();
 	return (
 		<div { ...blockProps }>
+			<InspectorControls>
+				<PanelBody title="Pixel Art Settings">
+					<RangeControl
+						label="Size"
+						value={ size }
+						onChange={ ( size ) => setAttributes( { size } ) }
+						min={ 64 }
+						max={ 512 }
+					/>
+				</PanelBody>
+			</InspectorControls>
+			<ServerSideRender block="rbl/pixel-art" attributes={ { size } } />
 		</div>
 	);
 }
