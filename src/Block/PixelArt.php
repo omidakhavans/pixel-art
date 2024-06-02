def023952e66eb22dd815dfc9b7c6f6669f8ef8f
Omid <omidakingroid@gmail.com>
2024-06-02 03:39:23 +0300

    Utelize cache on render block and enhance the consistency

--- src/Block/PixelArt.php
+++ src/Block/PixelArt.php
@@ -1,15 +1,16 @@
 <?php
 /**
- * Register custom block.
+ * Plugin initializer.
  *
- * @package   Country_Card
- * @copyright 2024 XWP
+ * @package   pixel Art
+ * @copyright 2024 RBL
  * @license   GNU General Public License 3.0
  */
 
 namespace RBL\Pixel_Art\Block;
 
 use RBL\Pixel_Art\Interfaces\Initializer;
+use RBL\Pixel_Art\CacheHandler;
 
 /**
  * Block module.
@@ -24,7 +25,7 @@ class PixelArt implements Initializer {
 	 * @return void
 	 */
 	public function load_hooks(): void {
-		add_action( 'init', array( __CLASS__, 'register_block' ) );
+		add_action( 'init', array( $this, 'register_block' ) );
 	}
 
 	/**
@@ -38,7 +39,7 @@ class PixelArt implements Initializer {
 		register_block_type_from_metadata(
 			PIXEL_ART_PATH . '/build/block/',
 			array(
-				'render_callback' => array( __CLASS__, 'render' ),
+				'render_callback' => array( self::class, 'render' ),
 				'attributes'      => array(
 					'size' => array(
 						'type'    => 'number',
@@ -54,22 +55,32 @@ class PixelArt implements Initializer {
 	 *
 	 * @since 1.0
 	 *
-	 * @param array $attributes Block attributes.
+ 	 * @param array<string,mixed> $attributes Block attributes.
 	 * @return string Rendered block content.
 	 */
 	public static function render( array $attributes ): string {
-		$pixels = get_option( 'pad_pixel_art', array_fill( 0, 256, 'transparent' ) );
+		$cache_handler = new CacheHandler( 'pixel_art_option' );
+		$pixels        = $cache_handler->get( 'pad_pixel_art', array_fill( 0, 256, 'transparent' ) );
+
+		if ( ! is_array( $pixels ) ) {
+			$decoded_pixels = json_decode( (string) $pixels, true );
+			if ( json_last_error() === JSON_ERROR_NONE && is_array( $decoded_pixels ) ) {
+				$pixels = $decoded_pixels;
+			} else {
+				$pixels = array_fill( 0, 256, 'transparent' );
+			}
+		}
 
 		// Ensure $attributes['size'] is set, otherwise fallback to default value.
-		$size = isset( $attributes['size'] ) ? $attributes['size'] : 128;
+		$size = isset( $attributes['size'] ) ? (int) $attributes['size'] : 128;
 
-		$svg       = '<svg width="' . esc_attr( $size ) . '" height="' . esc_attr( $size ) . '" viewBox="0 0 320 320">';
-		$pixelSize = 20;
+		$svg        = '<svg width="' . esc_attr( (string) $size ) . '" height="' . esc_attr( (string) $size ) . '" viewBox="0 0 320 320">';
+		$pixel_size = 20;
 
-		foreach ( json_decode( $pixels, true ) as $index => $color ) {
-			$x    = ( $index % 16 ) * $pixelSize;
-			$y    = floor( $index / 16 ) * $pixelSize;
-			$svg .= '<rect width="' . $pixelSize . '" height="' . $pixelSize . '" x="' . $x . '" y="' . $y . '" fill="' . esc_attr( $color ) . '"/>';
+		foreach ( $pixels as $index => $color ) {
+			$x    = ( $index % 16 ) * $pixel_size;
+			$y    = floor( $index / 16 ) * $pixel_size;
+			$svg .= '<rect width="' . esc_attr( (string) $pixel_size ) . '" height="' . esc_attr( (string) $pixel_size ) . '" x="' . esc_attr( (string) $x ) . '" y="' . esc_attr( (string) $y ) . '" fill="' . esc_attr( (string) $color ) . '"/>';
 		}
 
 		$svg .= '</svg>';
